7360cdfd8759d324ebc9a7f933ba71b021451381
fisch0920 <fisch0920@gmail.com>
2014-12-07 04:11:45 -0500

    removed GA tracking; updated style; improved robustness; incorporated chrome download name workaround

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
-This extension places a download button next to songs on HypeMachine allowing you to download them!
+This extension places a download button next to all playable songs on HypeMachine, allowing you to download them for free!
 
 It's super easy to install the extension by following these steps:
 
 1 - Download the ZIP and extract it
 
-2 - Go to Options -> Tools -> Extensions (Chrome wrench button)
+2 - Go to Chrome -> Preferences -> Extensions
 
 3 - Enable Developer Mode 
 
 4 - Click Load Unpacked Extension and select unzipped folder
 
-5- You should now be able to click on the download arrow next to the songs and enjoy them later one !
+5 - You should now be able to click on the download arrow next to songs and enjoy them later on!
 
-I'd like to add thanks for everyone who's supported the extension and sent me thank you e-mails.
-
-Cheers,
-Farid Zakaria
+Original extension by Farid Zakaria.
--- extension.js
+++ extension.js
@@ -1,136 +1,132 @@
-// Extension main script
 
-var stylesheetUrl = chrome.extension.getURL("hypestyles.css");
+(function () {
+  var stylesheetUrl = chrome.extension.getURL("hypestyles.css")
 
-// This is all the JS that will be injected in the document body
-var main = function() {
+  // This is all the JS that will be injected in the document body
+  var main = function () {
 
-  /**
-   * Return outerHTML for the first element in a jQuery object,
-   * or an empty string if the jQuery object is empty;  
-   */
-  jQuery.fn.outerHTML = function() {
-     return (this[0]) ? this[0].outerHTML : '';  
-  };
-  
-   /**
-   * Utility to wrap the different behaviors between W3C-compliant browsers
-   * and IE when adding event handlers.
-   *
-   * @param {Object} element Object on which to attach the event listener.
-   * @param {string} type A string representing the event type to listen for
-   *     (e.g. load, click, etc.).
-   * @param {function()} callback The function that receives the notification.
-   */
-   function addListener(element, type, callback) {
-    if (element.addEventListener) element.addEventListener(type, callback);
-    else if (element.attachEvent) element.attachEvent('on' + type, callback);
-   }
+    /**
+     * Return outerHTML for the first element in a jQuery object,
+     * or an empty string if the jQuery object is empty
+     */
+    jQuery.fn.outerHTML = function () {
+      return (this[0]) ? this[0].outerHTML : ''
+    }
 
-   /***
-   * The name _dlExtGATracker is chosen so as not to conflict with HypeMachine's possible
-   * use of Google Analytics object.
-   */
-   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-   })(window,document,'script','//www.google-analytics.com/analytics.js','_dlExtGATracker');
+    function generateRandomString () {
+      var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz"
+      var str = ""
+      for (var i = 0; i < 8; i++) {
+        var rnum = Math.floor(Math.random() * chars.length)
+        str += chars.charAt(rnum)
+      }
+      return str
+    }
 
-   _dlExtGATracker('create', 'UA-44161401-1', 'hypem.com');
-   _dlExtGATracker('send', 'pageview');
+    // Here is where we can randomly generate the name of the styles to avoid them checking for specific names.
+    // Can add more here if they check additional names.
+    // ref:http://jonraasch.com/blog/javascript-style-node
+    var triArrowString = generateRandomString()
+    var css = document.createElement('style')
+    css.type = 'text/css'
+    var styles = '.'+triArrowString+'{ width: 0; height: 0; border-left: 9px solid transparent; border-right: 9px solid transparent;	border-top: 10px solid #494949; }'
+    styles += '.arrow:hover .'+triArrowString+'{ border-top: 10px solid #0063DC; }'
 
-	//Random String generator.
-    var generator = function()
-    {
-    	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-		var string_length = 8;
-		var randomstring = "";
-		for (var i=0; i<string_length; i++) {
-			var rnum = Math.floor(Math.random() * chars.length);
-			randomstring += chars.charAt(rnum);
-		}
-		return randomstring;
-    };
-    
-    //Here is where we can randomly generate the name of the styles to avoid them checking for specific names.
-    //Can add more here if they check additional names.
-    //ref:http://jonraasch.com/blog/javascript-style-node
-    var triArrowString = generator();
-    var css = document.createElement('style');
-    css.type = 'text/css';
-    var styles = '.'+triArrowString+'{ width: 0; height: 0; border-left: 9px solid transparent; border-right: 9px solid transparent;	border-top: 10px solid #494949; }';
-    styles += '.arrow:hover .'+triArrowString+'{ border-top: 10px solid #0063DC; }';
-    
-    //done this way for IE aparantly.
-    if (css.styleSheet) css.styleSheet.cssText = styles;
-    else css.appendChild(document.createTextNode(styles) );
-    
-    document.getElementsByTagName("head")[0].appendChild(css);
-    
-    
-	// Adds a download button next to each track
-	var buttonScript = function() {
-		// Wait for the tracks script to load
-		var tracks = window.displayList['tracks'];
-		 
-    if (tracks === undefined || tracks.length < 1) {
-		 	setTimeout(buttonScript, 1);
-		} 
-    else {
-			// Check if this particular page has been processed
-			// through a previous call
-      if (jQuery('.dl').length < tracks.length) {
-				jQuery('ul.tools').each(function(index, track) {
-          var song = tracks[index];
-          var title = song.song;
-          var artist = song.artist;
-          var id = song.id;
-          var key = song.key;
-          var hasDownloadButton = jQuery(track).data("hasDownloadButton");
-          if (typeof(hasDownloadButton) === 'undefined' || !hasDownloadButton){
-            jQuery.getJSON("/serve/source/"+ id + "/" + key, function(data) {
-              var download_url = data.url;
-              var download_button = document.createElement("a");
-              download_button.target = "_top";
-              download_button.href = download_url;
-              download_button.className = "DownloadSongButton";
-              download_button.download = artist + ' - ' + title + '.mp3';
-              download_button.innerHTML = '<table class="arrow"><tr><td><div class="rect-arrow"></div></td></tr><tr><td class="'+triArrowString+     '"></td></tr></table>';
-              jQuery(track).prepend('<li class="dl"><table class="spacer"></table>' + jQuery(download_button)[0].outerHTML + '</li>');
-            });
-            jQuery(track).data("hasDownloadButton", true);
+    // handle inserting stylesheet for IE
+    if (css.styleSheet) css.styleSheet.cssText = styles
+    else css.appendChild(document.createTextNode(styles))
+
+    document.getElementsByTagName("head")[0].appendChild(css)
+
+    // Adds a download button next to each track
+    function initDownloadButtons () {
+      // Wait for the tracks script to load
+      var tracks = window.displayList && window.displayList.tracks
+
+      if (!tracks) {
+        setTimeout(initDownloadButtons, 50)
+      } else {
+        jQuery('ul.tools').each(function (index, track) {
+          var song = tracks[index]
+          if (!song || !song.id || !song.key) {
+            console.log('warning unable to lookup song', index, track)
+            return
           }
-          
-        });//each		
+
+          var title  = song.song
+          var artist = song.artist
+          var name   = ''
+
+          if (title || artist) {
+            if (title && artist) {
+              name = artist + ' - ' + title
+            } else {
+              name = (title || artist)
+            }
+          }
+
+          if (!jQuery(track).data("hasDownloadButton")) {
+            jQuery.getJSON("/serve/source/" + song.id + "/" + song.key, function (data) {
+              var downloadButton = document.createElement("a")
+              downloadButton.target = "_top"
+              downloadButton.href = data.url
+              downloadButton.className = "DownloadSongButton"
+              downloadButton.download = name.replace(/\./g, '') + '.mp3'
+              downloadButton.innerHTML = '<table class="arrow"><tr><td><div class="rect-arrow"></div></td></tr><tr><td class="' + triArrowString + '"></td></tr></table>'
+              jQuery(track).prepend('<li class="dl"><table class="spacer"></table>' + jQuery(downloadButton)[0].outerHTML + '</li>')
+            })
+
+            jQuery(track).data("hasDownloadButton", true)
+          }
+        })
       }
     }
-  };//buttonscript
-  
-  
-  jQuery('ul.tools').on('click', '.DownloadSongButton', function() {
-    console.log( "Downloading - " + jQuery(this)[0].download );
-    _dlExtGATracker('send', 'event', 'download', 'click', 'song-downloads', 1);
-  });
-	
-	// Run it right away
-	buttonScript();
-  
-  jQuery(document).ajaxComplete(function(event,request, settings){
-		buttonScript();
-  });
-  
-};
 
-// Lets create the script objects
-var injectedScript = document.createElement('script');
-injectedScript.type = 'text/javascript';
-injectedScript.text = '('+main+')("");';
-(document.body || document.head).appendChild(injectedScript);
+    // workaround for filename downloads issue
+    // https://code.google.com/p/chromium/issues/detail?id=373182
+    function downloadFile (sUrl, fileName) {
+      var xhr = new XMLHttpRequest()
+      xhr.open("GET", sUrl, true)
+      xhr.responseType = "blob"
+      xhr.onload = function () {
+        var res = xhr.response
+        var blob = new Blob([ res ], { type: "audio/mp3" })
+        var url = window.URL.createObjectURL(blob)
+        var a = document.createElement("a")
+        a.href = url
+        a.download = fileName
+        a.style = "display: none"
+        a.click()
+        window.URL.revokeObjectURL(url)
+      }
+      xhr.send()
+    }
 
-//Lets create the CSS object. This has to be done this way rather than the manifest.json
-//because we want to override some of the CSS properties so they must be injected after.
-var injectedCSS = document.createElement('link');
-injectedCSS.type = 'text/css';
-injectedCSS.rel = 'stylesheet';
-injectedCSS.href = stylesheetUrl;
-(document.body || document.head).appendChild(injectedCSS);
+    jQuery('ul.tools').on('click', '.DownloadSongButton', function (e) {
+      var anchor = e.currentTarget
+      console.log("Downloading - " + anchor.download)
+      downloadFile(anchor.href, anchor.download)
+      return false
+    })
+
+    initDownloadButtons()
+    jQuery(document).ajaxComplete(initDownloadButtons)
+  }
+
+  var top = document.body || document.head
+
+  // create the script objects
+  var injectedScript  = document.createElement('script')
+  injectedScript.type = 'text/javascript'
+  injectedScript.text = '(' + main + ')("");'
+  top.appendChild(injectedScript)
+
+  // Lets create the CSS object. This has to be done this way rather than the manifest.json
+  // because we want to override some of the CSS properties so they must be injected after.
+  var injectedCSS  = document.createElement('link')
+  injectedCSS.type = 'text/css'
+  injectedCSS.rel  = 'stylesheet'
+  injectedCSS.href = stylesheetUrl
+  top.appendChild(injectedCSS)
+})();
+
